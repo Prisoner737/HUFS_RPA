1f997e40f4d9f034569550a233e853afc1cdaedd
harry <rjwharry2003@gmail.com>
2019-08-24 00:21:10 +0900

    Test

--- RPA_SummerProj/Program.cs
+++ RPA_SummerProj/Program.cs
@@ -17,6 +17,7 @@ namespace RPA_SummerProj
 
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
