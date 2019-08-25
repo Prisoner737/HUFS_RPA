cb474fda33d6b05d42ee036dfaec37f31ac43e17
Ko, Byung Hyun <prisoner737@gmail.com>
2019-08-26 00:25:40 +0900

    Add Outlook delete and move Activitis

--- RPA_SummerProj/core/implement/MailManager.cs
+++ RPA_SummerProj/core/implement/MailManager.cs
@@ -40,7 +40,6 @@ namespace RPA_SummerProj.core.implement
 
         public void receiveMail()
         {
-            int i = 0;
             try
             {
                 _ns = _app.GetNamespace("MAPI");
@@ -48,14 +47,13 @@ namespace RPA_SummerProj.core.implement
                 _ns.SendAndReceive(true);
                 foreach (Outlook.MailItem item in inbox.Items)
                 {
-                    Console.WriteLine("Subject : " + item.Subject);
-                    Console.WriteLine("Sender Name : " + item.SenderName);
-                    Console.WriteLine("Body : " + item.HTMLBody);
-                    Console.WriteLine("Data : " + item.SentOn.ToLongDateString() + " " + item.SentOn.ToLongTimeString());
-                    i++;
-
-                    if (i == 10)
-                        break;
+                    if(item.UnRead == true)
+                    {
+                        Console.WriteLine("Subject : " + item.Subject);
+                        Console.WriteLine("Sender Name : " + item.SenderName);
+                        Console.WriteLine("Body : " + item.HTMLBody);
+                        Console.WriteLine("Data : " + item.SentOn.ToLongDateString() + " " + item.SentOn.ToLongTimeString());
+                    }
                 }
             }
             catch (Exception ex)
@@ -64,6 +62,78 @@ namespace RPA_SummerProj.core.implement
             }
         }
 
+        public void deleteByName(string name)
+        {
+            try
+            {
+                _ns = _app.GetNamespace("MAPI");
+                inbox = _ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                _ns.SendAndReceive(true);
+                foreach (Outlook.MailItem item in inbox.Items)
+                {
+                    if (item.SenderName == name)
+                    {
+                        item.Delete();
+                        Console.WriteLine("Subject : " + item.Subject + "from : " + item.SenderName + "is deleted");
+                    }
+                        
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
+        public void deleteBySubjectKeywords(string subj)
+        {
+            try
+            {
+                _ns = _app.GetNamespace("MAPI");
+                inbox = _ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                _ns.SendAndReceive(true);
+                foreach (Outlook.MailItem item in inbox.Items)
+                {
+                    if (item.Subject.Contains(subj))
+                    {
+                        Console.WriteLine("Subject : " + item.Subject + "from : " + item.SenderName + "is deleted");
+                        item.Delete();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void moveMailFolder(string folderName, string targetMail)
+        {
+            Outlook.MAPIFolder inBox = _ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            Outlook.Items items = (Outlook.Items)inBox.Items;
+            Outlook.MailItem moveMail = null;
+            items.Restrict("[UnRead] = true");
+            Outlook.MAPIFolder destFolder = inBox.Folders[folderName];
+            foreach (object eMail in items)
+            {
+                try
+                {
+                    moveMail = eMail as Outlook.MailItem;
+                    if (moveMail != null)
+                    {
+                        string titleSubject = (string)moveMail.Subject;
+                        if (titleSubject.IndexOf(targetMail) > 0)
+                        {
+                            moveMail.Move(destFolder);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
     }
 }
--- RPA_SummerProj/core/module/OutlookDeleteBySenderName.cs
+++ RPA_SummerProj/core/module/OutlookDeleteBySenderName.cs
@@ -4,14 +4,19 @@ using RPA_SummerProj.core.implement;
 namespace RPA_SummerProj.core.module
 {
 
-    public sealed class OutlookShowMSG : CodeActivity
+    public sealed class OutlookDeleteBySenderName : CodeActivity
     {
+        // 형식 문자열의 작업 입력 인수를 정의합니다.
+        public InArgument<string> senderName { get; set; }
+
         // 작업 결과 값을 반환할 경우 CodeActivity<TResult>에서 파생되고
         // Execute 메서드에서 값을 반환합니다.
         protected override void Execute(CodeActivityContext context)
         {
+            // 텍스트 입력 인수의 런타임 값을 가져옵니다.
+            string sndName = context.GetValue(this.senderName);
             MailManager myMail = new MailManager();
-            myMail.receiveMail();
+            myMail.deleteByName(sndName);
         }
     }
 }
