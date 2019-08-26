64ee41cb1140a266ab4a51f359810f5d79537498
고병현 <prisoner737@o365.hufs.ac.kr>
2019-08-26 13:26:33 +0900

    ADD "ROH's" EXCEL implement and activities

---
+++ RPA_SummerProj/core/module/ExcelAppend.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace RPA_SummerProj.core.module
+{
+
+    public sealed class ExcelAppend : CodeActivity
+    {
+        public InArgument<object> instance { get; set; }
+        public InArgument<string> instanceName { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+
+            Console.WriteLine("Append");
+
+            var EngineInstance = (Program)instance.Get(context);
+            string InstanceName = instanceName.Get(context);
+            object excel;
+            //Console.WriteLine(sendingInstance.GetType());
+
+            if (EngineInstance.appInstance.TryGetValue(InstanceName, out excel))
+            {
+                Console.WriteLine("Success");
+                //Excel.Workbook eWB = (Excel.Workbook)excel;
+                Excel.Application eXL = (Excel.Application)excel;
+                Excel.Workbook eWB = eXL.ActiveWorkbook;
+                Excel.Worksheet eWS = eWB.Worksheets.Item[eWB.Worksheets.Count];
+                Excel.Worksheet newWS = eWB.Worksheets.Add();
+                newWS.Move(After: eWS);
+                //ReleaseExcelObject(eWB);
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
---
+++ RPA_SummerProj/core/module/ExcelCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace RPA_SummerProj.core.module
+{
+
+    public sealed class Create : CodeActivity
+    {
+        public InArgument<object> instance { get; set; }
+        public InArgument<string> instanceName { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            Console.WriteLine("Create");
+            var EngineInstance = (Program)instance.Get(context);
+            string InstanceName = instanceName.Get(context);
+            Excel.Application eXL = new Excel.Application();
+            Excel.Workbook eWB = eXL.Workbooks.Add();
+            eXL.Visible = true;
+            //EngineInstance.appInstance.Add("Excel", eWB);
+            EngineInstance.appInstance.Add(InstanceName, eXL);
+        }
+    }
+}
